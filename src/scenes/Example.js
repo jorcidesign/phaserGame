bad91465d2bb7f55faffc6affa4421418b1c00bd
jorcidesign <jorci.design@gmail.com>
2023-06-19 04:07:23 -0500

    ./assets

--- src/scenes/Example.js
+++ src/scenes/Example.js
@@ -6,8 +6,8 @@ export class Example extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('block', 'assets/circle.png');
-        this.load.image('wall','assets/wall.png');
+        this.load.image('block', './assets/circle.png');
+        this.load.image('wall','./assets/wall.png');
     }
 
     create ()
