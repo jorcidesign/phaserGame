105d1e60c11a6510c13322c2512b5b6a91735fb0
jorcidesign <jorci.design@gmail.com>
2023-06-19 03:20:29 -0500

    primera version

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { Example } from "./scenes/Example";
+
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game-container',
+    backgroundColor: '#fff',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true
+        }
+    },
+    scene: Example
+};
+
+const game = new Phaser.Game(config);
---
+++ src/scenes/Example.js
@@ -0,0 +1,51 @@
+export class Example extends Phaser.Scene
+{
+    player;
+    cursors;
+    walls;
+
+    preload ()
+    {
+        this.load.image('block', 'assets/circle.png');
+        this.load.image('wall','assets/wall.png');
+    }
+
+    create ()
+    {
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        
+
+        this.player = this.physics.add.image(400, 300, 'block');
+        
+        this.walls = this.physics.add.image(120,200,'wall');
+        this.player.setCollideWorldBounds(true);
+        this.walls.setImmovable(true); 
+        this.walls.setCollideWorldBounds(true);
+        this.physics.add.collider(this.player, this.walls);
+    }
+
+    update ()
+    {
+        this.player.setVelocity(0);
+
+        if (this.cursors.left.isDown)
+        {
+            this.player.setVelocityX(-300);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setVelocityX(300);
+        }
+
+        if (this.cursors.up.isDown)
+        {
+            this.player.setVelocityY(-300);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.setVelocityY(300);
+        }
+    }
+}
+
